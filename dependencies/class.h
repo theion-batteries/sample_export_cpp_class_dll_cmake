6ac0984f7caea72da76fa8c3acb609026cb71c8e
sami-theion <sami@theion.de>
2023-01-20 14:46:26 +0100

    added class c

--- dependencies/class.h
+++ dependencies/class.h
@@ -8,6 +8,7 @@
  * @copyright Copyright (c) 2023
  * 
  */
+#pragma once
 #include <iostream>
 #include <memory>
 // convention: HEADER_EXPORTS
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,14 @@
-#include "class.h"
+#include "client.h"
 
 int main()
 {
-    // shared ptr
-    auto objS = GetInterfaceSharedPtr();
-    objS=std::make_shared<DerivedA>();
-    objS->add(1,2);
-    objS->getSum();
-    objS=std::make_shared<DerivedB>();
-    objS->add(1,3);
-    objS->getSum();
-    // unqiue ptr
-    std::cout<<"unique"<<std::endl;
-    auto objU = GetInterfaceUniquePtr();
-    objU=std::make_unique<DerivedA>();
-    objU->add(1,2);
-    objU->getSum();
-    objU=std::make_unique<DerivedB>();
-    objU->add(1,3);
-    objU->getSum();
+    client c;
+    DerivedA a;
+    a.add(5,2);
+    a.getSum();
+    DerivedB b;
+    b.add(3,6);
+    b.getSum();
 
     return 0;
 }
